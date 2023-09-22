3e298b78cdf4691f3c779e097a4d445f67f25561
Kerb9_ <lukinkirill61@gmail.com>
2023-09-22 21:58:36 +0300

    fixing right and cpp

--- right.cpp
+++ right.cpp
@@ -49,17 +49,16 @@ void sort(pair<char, int> arr[], pair<char, int> buffer[], int left, int right)
     return;
 }
 
-int right(string &s, int arr[]) {
-    //long long str_weight = 0;
-    
+int right(string &s, int arr[]) { // подсчет веса любой строки
     map<char, int> letter_count;
     pair<char, int> letter_weight[26];
     pair<char, int> buffer[26];
+    int size = s.length();
     for (int i = 0; i < 26; i++) {
         letter_weight[i] = make_pair(static_cast<char>(i + 97), arr[i]);
         buffer[i] = make_pair(static_cast<char>(i + 97), arr[i]);
     }
-    for (int i = 0; i < s.length(); i++) {
+    for (int i = 0; i < size; i++) {
         if (letter_count.count(s[i]) == 0){
             letter_count[s[i]] = 1;
         } else {
@@ -67,24 +66,14 @@ int right(string &s, int arr[]) {
         }
     }
     sort(letter_weight, buffer, 0, 25);
-
-    /*for (auto pair : letter_weight) {
-        str_weight += pair.second*letter_count[pair.first]/2;
-    }*/
     long long maxWeight = 0;
-    int maxDiff = s.length() - 1;
-    for (auto p = 0; p < 26 ; p++) {
-        while (letter_count[p.first] > 1) {
-            maxWeight += maxDiff * p.second;
+    int maxDiff = size - 1;
+    for (int p = 0; p < 26 ; p++) {
+        while (letter_count[letter_weight[p].first] > 1) {
+            maxWeight += maxDiff * letter_weight[p].second;
             maxDiff -= 2;
+            letter_count[letter_weight[p].first] -= 2;
         }
     }
     return maxWeight;
-}
-
-int main() {
-    string s;
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26};
-    cout << right(s, arr);
-    return 0;
 }
